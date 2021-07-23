14826c6a6fab4eec0cbe9923033a40d7dc1a5578
Tyler <tylershelton99@gmail.com>
2021-07-23 01:18:40 +0000

    Added missing ; in head.js and added comments to both head.js and assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,7 @@
 // Function Implementation
 const assertEqual = function(actual, expected) {
+
+  // if actual and expected are equal, Pass, otherwise Fail
   actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
     console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
 };
--- head.js
+++ head.js
@@ -1,13 +1,17 @@
 // Function Implementation
 const assertEqual = function(actual, expected) {
+
+  // if actual and expected are equal, Pass, otherwise Fail
   actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) :
     console.log(`🚨 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 const head = function(array) {
+  // return the first item from the array that was passed into the head function
   return array[0];
-}
+};
 
+// Test Function Calls
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 console.log(head([5]));
