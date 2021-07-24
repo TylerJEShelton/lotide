547497ff8c99f858d1e3213acd135118bc928ded
Tyler <tylershelton99@gmail.com>
2021-07-24 05:28:55 +0000

    Created and implemented assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  // if actual and expected are equal, Pass, otherwise Fail
+  eqArrays(actual, expected) ? console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`) :
+    console.log(`🚨 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+assertArraysEqual([1, 2], [1, 2]);
+assertArraysEqual([1, 2], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [2, 2, 3]);
