13a70e35e76003e7af5e217eb08b0549846d73df
Tyler <tylershelton99@gmail.com>
2021-08-09 19:27:23 +0000

    Added assertion checks

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  // if actual and expected are equal, Pass, otherwise Fail
+  eqArrays(actual, expected) ? console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`) :
+    console.log(`🚨 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
 const takeUntil = function(array, callback) {
   const result = [];  // create an empty array that will store the results
   // loop through all of the elements in the array
@@ -17,8 +37,12 @@ const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
 
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
