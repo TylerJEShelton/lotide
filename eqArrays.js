3727aae3c5cf35ed9da550983b17ec5d0d29fdf0
Tyler <tylershelton99@gmail.com>
2021-08-18 04:00:03 +0000

    removed some unnecessary comments and eliminated the nested if statement

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,11 @@
 const eqArrays = function(array1, array2) {
   // if the arrays are not the same length, return false
-  if (array1.length === array2.length) {
-    // loop through each element of both arrays
-    for (let i = 0; i < array1.length; i++) {
-      // if the array elements at index i do not match return false
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    // if the array elements at index i do not match return false
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-  } else {
-    return false;
   }
   return true;  // if the array iterations complete without finding a mismatch, return true
 };
